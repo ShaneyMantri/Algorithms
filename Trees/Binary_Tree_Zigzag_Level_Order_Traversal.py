f9126bf206eab07f273bda9d7df2d963ddfcb022
Shaney Mantri <45652188+ShaneyMantri@users.noreply.github.com>
2020-07-22 15:30:49 +0530

    Update Binary_Tree_Zigzag_Level_Order_Traversal.py

--- Trees/Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ Trees/Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -37,10 +37,6 @@ class Solution:
         d1.appendleft((root,0))
         res = {}
         while d1:
-            # print(d1)
-            # print(d2)
-            # print(res)
-            # print("\n")
             temp = d1.popleft()
             if temp[1]%2==0:
                 if temp[0].left:
@@ -69,3 +65,61 @@ class Solution:
             ret.append(v)
         print(ret)
         return ret
+
+    
+    ## METHOD 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        level = 0
+        if not root:
+            return 
+        d1 = deque([(root, 0)])
+        d2 = deque([])
+        
+        
+        d = {}
+        level=1
+        while d1 or d2:
+            # print(d1,d2)
+            print("D1",d1)
+            
+            if level%2==1:
+                if not d1:
+                    break
+                while d1:
+                    n,l = d1.popleft()
+                    if n.left:
+                        d2.append((n.left, l+1))
+                    if n.right:
+                        d2.append((n.right, l+1))
+                    if l in d:
+                        d[l].append(n.val)
+                    else:
+                        d[l]=[n.val]
+                level+=1
+            print(d2)
+            if level%2==0:
+                if not d2:
+                    break
+                while d2:
+                    n,l = d2.pop()
+                    if n.right:
+                        d1.appendleft((n.right, l+1))
+                    if n.left:
+                        d1.appendleft((n.left, l+1))
+                    if l in d:
+                        d[l].append(n.val)
+                    else:
+                        d[l]=[n.val]
+                level+=1
+        return d.values()
+                
+            
+        
