ce451cc5085683a4cf2ce193129525acc3c009f1
Shaney Mantri <45652188+ShaneyMantri@users.noreply.github.com>
2020-08-09 16:26:11 +0530

    Create Find_Kth_Bit_in_Nth_Binary_String.py

---
+++ Strings/Find_Kth_Bit_in_Nth_Binary_String.py
@@ -0,0 +1,48 @@
+"""
+Given two positive integers n and k, the binary string  Sn is formed as follows:
+
+S1 = "0"
+Si = Si-1 + "1" + reverse(invert(Si-1)) for i > 1
+Where + denotes the concatenation operation, reverse(x) returns the reversed string x, and invert(x) inverts all the bits in x (0 changes to 1 and 1 changes to 0).
+
+For example, the first 4 strings in the above sequence are:
+
+S1 = "0"
+S2 = "011"
+S3 = "0111001"
+S4 = "011100110110001"
+Return the kth bit in Sn. It is guaranteed that k is valid for the given n.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 1
+Output: "0"
+Explanation: S3 is "0111001". The first bit is "0".
+Example 2:
+
+Input: n = 4, k = 11
+Output: "1"
+Explanation: S4 is "011100110110001". The 11th bit is "1".
+Example 3:
+
+Input: n = 1, k = 1
+Output: "0"
+Example 4:
+
+Input: n = 2, k = 3
+Output: "1"
+"""
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        temp = ''
+        s = '0'
+        i = 1
+        while i<n:
+            t1 = bin(int(s,2)^(2**(len(s))-1))[2:]
+            temp = s+'1'+t1[::-1]
+            s = temp
+            i+=1
+        return s[k-1]
+        
