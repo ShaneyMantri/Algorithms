966792a64f94ba0eb69f36afd6e4a0d2e03eff29
Shaney Mantri <45652188+ShaneyMantri@users.noreply.github.com>
2020-08-15 19:35:08 +0530

    Update Greatest_Sum_Divisible_by_Three.py

--- DP/Greatest_Sum_Divisible_by_Three.py
+++ DP/Greatest_Sum_Divisible_by_Three.py
@@ -35,5 +35,17 @@ class Solution:
             dp[i][t1] = max(nums[i], dp[i][t1])
         
         return dp[-1][0]
+    
+## METHOD 2
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n==0:
+            return 0
+        dp = [0,0,0]
+        for num in nums:
+            for i in dp[:]:
+                dp[(i+num)%3] = max(dp[(i+num)%3], i+num)
+        return dp[0]
                 
         
