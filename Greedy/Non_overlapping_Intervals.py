3acc430a7f29ed095e582fb1ab28e8cf5e5f3050
Shaney Mantri <45652188+ShaneyMantri@users.noreply.github.com>
2020-08-15 19:17:50 +0530

    Update Non_overlapping_Intervals.py

--- Greedy/Non_overlapping_Intervals.py
+++ Greedy/Non_overlapping_Intervals.py
@@ -20,7 +20,7 @@ Output: 0
 Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
 
 """
-
+## METHOD 1 16/18 - WA
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         if len(intervals)==0 or len(intervals)==1:
@@ -40,5 +40,25 @@ class Solution:
                 stat[j]=True
         return remove
                     
-                        
+
+## METHOD 2 AC
+class Solution:
+    def unique(self, a):
+        sortedIntervals = sorted(a, key=lambda x:x[1])
+        end = sortedIntervals[0][1]
+        remove = 0
+        for i in range(1, len(sortedIntervals)):
+            if sortedIntervals[i][0]<end:
+                remove+=1
+            else:
+                end=sortedIntervals[i][1]
+        return remove
+            
+        
+        
+        
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        if 0<=len(intervals)<=1:
+            return 0
+        return self.unique(intervals)
         
