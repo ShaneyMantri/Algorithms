86e26d0eaf4e06a0ce3e34a9548f423345a0def2
Shaney Mantri <45652188+ShaneyMantri@users.noreply.github.com>
2020-08-24 12:03:33 +0530

    Update Jump_Game_II.py

--- BFS&DFS/Jump_Game_II.py
+++ BFS&DFS/Jump_Game_II.py
@@ -13,42 +13,23 @@ Explanation: The minimum number of jumps to reach the last index is 2.
     Jump 1 step from index 0 to 1, then 3 steps to the last index.
 """
 
-## TLE BFS 91/92
-
 from collections import deque
 class Solution:
-    def makeGraph(self, nums, n):
-        d = {}
-        for i in range(n-1):
-            j = i+1
-            k = nums[i]
-            d[i]=[]
-            while j<n and k>0:
-                d[i].append(j)
-                j+=1
-                k-=1
-                
-            j = i-1
-            k = nums[i]
-            while j>=0 and k>0:
-                d[i].append(j)
-                j+=1
-                k=-1
-        return d
-    
-    
     def jump(self, nums: List[int]) -> int:
-        g = self.makeGraph(nums, len(nums))
+        if len(nums)<=1:
+            return 0
         d = deque([(0,0)])
         n = len(nums)
         v = [0]*n
+        v[0]=1
         while d:
             ind, val = d.popleft()
-            if ind==(n-1):
-                return val
-            for i in g[ind]:
+            if ind+nums[ind]>=(n-1):
+                return val+1
+            for i in range(min(n-1, ind+nums[ind]),max(0, ind-nums[ind]-1), -1):
                 if v[i]==0:
                     v[i]=1
                     d.append((i, val+1))
+        return 
         
                 
