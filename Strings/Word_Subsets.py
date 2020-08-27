ff7264d7c15a7bd04026cac8b587f26529d5d311
Shaney Mantri <45652188+ShaneyMantri@users.noreply.github.com>
2020-08-27 10:42:33 +0530

    Update Word_Subsets.py

--- Strings/Word_Subsets.py
+++ Strings/Word_Subsets.py
@@ -1,7 +1,8 @@
 """
 We are given two arrays A and B of words.  Each word is a string of lowercase letters.
 
-Now, say that word b is a subset of word a if every letter in b occurs in a, including multiplicity.  For example, "wrr" is a subset of "warrior", but is not a subset of "world".
+Now, say that word b is a subset of word a if every letter in b occurs in a, including multiplicity.  
+For example, "wrr" is a subset of "warrior", but is not a subset of "world".
 
 Now say a word a from A is universal if for every b in B, b is a subset of a. 
 
